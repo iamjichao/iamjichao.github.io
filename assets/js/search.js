091836cf766b39755f037ca1911f028e2c3c7773
jichao <iamjichao@163.com>
2019-02-25 11:47:54 +0800

    update

--- assets/js/search.js
+++ assets/js/search.js
@@ -5,8 +5,8 @@ jQuery(function() {
                 $("#site_search").width(300);
             }
         }
-        // var searchbar_width = $("#site_search").width();
-        // $("#search_box").width(searchbar_width - 65);
+        var searchbar_width = $("#site_search").width();
+        $("#search_box").width(searchbar_width - 65);
     }
 
     adjust_search_box_width();
